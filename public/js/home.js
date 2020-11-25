cdc2ce754c428c3b79dca3d9ae8919a3b213626e
nurofsun <dev.nurahmat@gmail.com>
2020-11-26 06:29:11 +0700

    Update login page design

--- public/js/home.js
+++ public/js/home.js
@@ -1,1 +1,226 @@
-!function(t){var n={};function e(r){if(n[r])return n[r].exports;var o=n[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,e),o.l=!0,o.exports}e.m=t,e.c=n,e.d=function(t,n,r){e.o(t,n)||Object.defineProperty(t,n,{enumerable:!0,get:r})},e.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},e.t=function(t,n){if(1&n&&(t=e(t)),8&n)return t;if(4&n&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(e.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&n&&"string"!=typeof t)for(var o in t)e.d(r,o,function(n){return t[n]}.bind(null,o));return r},e.n=function(t){var n=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(n,"a",n),n},e.o=function(t,n){return Object.prototype.hasOwnProperty.call(t,n)},e.p="/",e(e.s=14)}({1:function(t,n,e){"use strict";e.d(n,"a",(function(){return r}));class r{constructor(t){this.number=t}convert(){this.number=this.number.toString().replace(/\./g,"");let t=3,n=this.number.length%t,e=Math.floor(this.number.length/t);if(this.number=this.number.split(""),0==n){if(1!=e){for(let n=0;n<e;n++)this.number.splice(this.number.length-t++,0,"."),t+=3;return this.number=this.number.join(""),this.number.substring(1)}return this.number.join("")}for(let n=0;n<e;n++)this.number.splice(this.number.length-t++,0,"."),t+=3;return this.number.join("")}clean(){return this.number=this.number.replace(/\./g,""),parseInt(this.number)}}},14:function(t,n,e){t.exports=e(15)},15:function(t,n,e){"use strict";e.r(n);var r=e(1);new Promise((function(t,n){var e=document.querySelectorAll(".amount");0!==e.length&&t(e),n("Cannot find amount elements")})).then((function(t){t.forEach((function(t){t.textContent=new r.a(t.textContent).convert()}))})),new Promise((function(t,n){window.allMonthPaymentHistory&&t(window.allMonthPaymentHistory),n(new Error("Cannot find payment history"))})).then((function(t){var n=d3.select("#payment-chart"),e=document.querySelector("#payment-chart").clientWidth,r=10,o=20,i=50,a=d3.scaleBand().domain(t.map((function(t){return t.month}))).rangeRound([i,e]).padding(.1),u=d3.scaleLinear().domain([0,d3.max(t,(function(t){return parseInt(t.amount)}))]).range([300-o,r]),c=n.append("svg").attr("height",300).attr("width",a.range()[1]);c.selectAll("g").data(t).join("g").attr("transform",(function(t){return"translate(".concat(a(t.month),", 0)")})).append("rect").attr("fill","#0CCE6B").attr("y",(function(t){return u(t.amount)})).attr("height",(function(t){return u(0)-u(t.amount)})).attr("width",a.bandwidth()),c.append("g").attr("transform","translate(0,".concat(300-o,")")).call(d3.axisBottom(a)),c.append("g").attr("transform","translate(".concat(i,",0)")).call(d3.axisLeft(u))})).catch((function(t){return console.log(t)}))}});
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./node_modules/.pnpm/rupiafjs@1.0.0/node_modules/rupiafjs/src/js/rupiaf.amd.js":
+/*!**************************************************************************************!*\
+  !*** ./node_modules/.pnpm/rupiafjs@1.0.0/node_modules/rupiafjs/src/js/rupiaf.amd.js ***!
+  \**************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Rupiaf; });
+class Rupiaf {
+    constructor(number) {
+        this.number = number;
+    }
+    convert() {
+        this.number = this.number.toString().replace(/\./g, '');
+        let step = 3,
+            sisa = this.number.length % step,
+            counter = Math.floor(this.number.length / step);
+        this.number = this.number.split('');
+        if (sisa == 0) {
+            if (counter != 1) {
+                for (let i=0; i < counter; i++) {
+                    this.number.splice(this.number.length - (step++),0,'.')
+                    step += 3;
+                }
+                this.number = this.number.join('');
+                return this.number.substring(1)
+            }
+            return this.number.join('');
+        } else {
+            for (let i=0; i < counter; i++) {
+                this.number.splice(this.number.length - (step++),0,'.')
+                step += 3;
+            }
+        return this.number.join('');
+        }
+    }
+    clean() {
+        this.number = this.number.replace(/\./g,'');
+        return parseInt(this.number)
+    }
+}
+
+
+
+/***/ }),
+
+/***/ "./resources/js/home.js":
+/*!******************************!*\
+  !*** ./resources/js/home.js ***!
+  \******************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var rupiafjs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! rupiafjs */ "./node_modules/.pnpm/rupiafjs@1.0.0/node_modules/rupiafjs/src/js/rupiaf.amd.js");
+
+
+function getAmounts() {
+  return new Promise(function (resolve, reject) {
+    var amountElements = document.querySelectorAll('.amount');
+
+    if (amountElements.length !== 0) {
+      resolve(amountElements);
+    }
+
+    reject('Cannot find amount elements');
+  });
+}
+
+getAmounts().then(function (elements) {
+  elements.forEach(function (element) {
+    element.textContent = new rupiafjs__WEBPACK_IMPORTED_MODULE_0__["default"](element.textContent).convert();
+  });
+});
+
+function getAllMonthPaymentHistory() {
+  return new Promise(function (resolve, reject) {
+    if (window.allMonthPaymentHistory) {
+      resolve(window.allMonthPaymentHistory);
+    }
+
+    reject(new Error('Cannot find payment history'));
+  });
+}
+
+function statisticYearlyChart() {
+  getAllMonthPaymentHistory().then(function (data) {
+    var chartContainer = d3.select('#payment-chart');
+    var height = 300;
+    var width = document.querySelector('#payment-chart').clientWidth;
+    var margin = {
+      top: 10,
+      right: 0,
+      bottom: 20,
+      left: 50
+    };
+    var x = d3.scaleBand().domain(data.map(function (d) {
+      return d.month;
+    })).rangeRound([margin.left, width]).padding(0.1);
+    var y = d3.scaleLinear().domain([0, d3.max(data, function (d) {
+      return parseInt(d.amount);
+    })]).range([height - margin.bottom, margin.top]);
+    var chart = chartContainer.append('svg').attr('height', height).attr('width', x.range()[1]);
+    var bar = chart.selectAll('g').data(data).join('g').attr('transform', function (d) {
+      return "translate(".concat(x(d.month), ", 0)");
+    });
+    bar.append('rect').attr('fill', '#0CCE6B').attr('y', function (d) {
+      return y(d.amount);
+    }).attr('height', function (d) {
+      return y(0) - y(d.amount);
+    }).attr('width', x.bandwidth());
+    chart.append("g").attr("transform", "translate(0,".concat(height - margin.bottom, ")")).call(d3.axisBottom(x));
+    chart.append("g").attr("transform", "translate(".concat(margin.left, ",0)")).call(d3.axisLeft(y));
+  })["catch"](function (err) {
+    return console.log(err);
+  });
+}
+
+statisticYearlyChart();
+
+/***/ }),
+
+/***/ 1:
+/*!************************************!*\
+  !*** multi ./resources/js/home.js ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /home/nur/Project/Web/maketin/resources/js/home.js */"./resources/js/home.js");
+
+
+/***/ })
+
+/******/ });
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,45 +5,55 @@
 @section('content')
     <main class="login vh-100">
         <div class="bg-white vh-100">
-        <div class="row vh-100 no-gutters">
-            <div class="col-12 col-md-6 bg-primary">
-
-            </div>
-            <div class="col-12 col-md-6">
-                <section class="login-form vh-100 d-flex align-items-center justify-content-center">
-                    <form action="{{ route('authenticate') }}" method="POST" class="flex-shrink-0 py-3">
-                        @csrf
-                        <div class="mb-3">
-                            <h3 class="h2 font-weight-bold">Welcome!</h3>
+            <div class="row vh-100 no-gutters">
+                <div class="col-12 col-md-6 bg-primary">
+                    <section class="content vh-100 d-flex flex-column justify-content-center">
+                        <div class="details p-4">
+                            <p class="title display-4 text-white font-weight-bold">
+                                Easily Manage The Payment Of Your School
+                            </p>
+                            <img width="500" src="{{ @asset('images/banner.svg') }}" alt="Easily Manage The Payment of Your School">
                         </div>
-                        <div class="mb-3">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
-                                </div>
-                                <input class="form-control" name="username" id="username" type="text" placeholder="Username">
+                    </section>
+                </div>
+                <div class="col-12 col-md-6 bg-light">
+                    <section class="login-form vh-100 d-flex align-items-center justify-content-center">
+                        <form action="{{ route('authenticate') }}" method="POST" class="flex-shrink-0 py-3 px-3 shadow-sm rounded-lg bg-white">
+                            @csrf
+                            <div class="mb-3">
+                                <h3 class="h2 font-weight-bold">Welcome Back</h3>
+                                <p class="small">Please login first to continue.</p>
                             </div>
-                        </div>
-                        <div class="mb-3">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <span class="input-group-text">
-                                        <i class="fas fa-key"></i>
-                                    </span>
+                            <div class="mb-3">
+                                <label class="label font-weight-bold">Username</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input class="form-control" name="username" id="username" type="text" placeholder="Username">
                                 </div>
-                                <input class="form-control" id="password" type="password" name="password" placeholder="Password">
                             </div>
-                        </div>
-                        <div class="mb-2">
-                            <button class="btn btn-primary" type="submit">
-                                <span class="font-weight-bold">Login</span>
-                                <span class="icon"><i class="fas fa-sign-in-alt"></i></span>
-                            </button>
-                        </div>
-                    </form>
-                </section>
+                            <div class="mb-3">
+                                <label class="label font-weight-bold">Password</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <i class="fas fa-key"></i>
+                                        </span>
+                                    </div>
+                                    <input class="form-control" id="password" type="password" name="password" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="mb-3 float-right">
+                                <button class="btn btn-primary" type="submit">
+                                    <span class="font-weight-bold">Login</span>
+                                    <span class="icon"><i class="fas fa-sign-in-alt"></i></span>
+                                </button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
             </div>
-        </div>
         </div>
     </main>
 @endsection
